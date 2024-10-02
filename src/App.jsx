93af06b3ef32e74be0a7f4ca8024b493323b8e38
U01A7DBC <aortega@opentrends.net>
2024-10-02 17:52:19 +0200

    add service and loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies.js";
 
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirtsInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirtsInput.current) {
+      isFirtsInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacía");
+      return;
+    }
+    if (search.length < 3) {
+      setError("La busqueda debe tener al menos 3 caracteres");
+      return;
+    }
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
+
 function App() {
-  const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies,loading } = useMovies({ search });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies();
+  };
+
+  const handleChange = (e) => {
+    updateSearch(e.target.value);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de Películas</h1>
-        <form className="form">
-          <input placeholder="Avengers,Stars Wars...." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            name="query"
+            placeholder="Avengers,Stars Wars...."
+          />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={movies} />
+        {loading ? <p>Cargando ...</p> : <Movies movies={movies} /> }
       </main>
     </div>
   );
